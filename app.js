8eaaabf96f4039604273823482ecb73c0c306540
daniel-aziz <91468647+daniel-aziz@users.noreply.github.com>
2022-02-23 11:37:23 +0200

    finshied api method for returning timestamp

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const express = require('express')
 const app = express();
+require('dotenv').config()
 
+app.get('/api/:date', (req, res) => {
+    let reqDate = req.params.date;
+    var resUTC, resUnix;
+    if (reqDate.includes('-')) {
+        resUTC = new Date(reqDate).toUTCString();
+        resUnix = Math.floor(new Date(reqDate).getTime());
+    } else {
+        resUTC = new Date(parseInt(reqDate)).toUTCString();
+        resUnix = Math.floor(new Date(parseInt(reqDate)).getTime());
+    }
+    res.status(200).json({ unix: resUnix, utc: resUTC })
+})
 
-app.listen()
+app.listen(process.env.SERVER_PORT, () => {
+    console.log(`Server is running at port ${process.env.SERVER_PORT}`)
+})
