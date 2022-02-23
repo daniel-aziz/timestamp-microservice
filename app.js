84027d05f902f666c12ba2da931a758b03740baf
daniel-aziz <91468647+daniel-aziz@users.noreply.github.com>
2022-02-23 21:20:33 +0200

    shorten methods and added another nothod for current date

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ const express = require('express')
 const app = express();
 require('dotenv').config()
 
+// Get the current date in unix and utc
+app.get('/api', (req, res) => {
+    res.status(200).json({ unix: new Date().getTime(), utc: new Date().toUTCString() })
+})
+
+// Get the user input date in unix and utc
 app.get('/api/:date', (req, res) => {
     let reqDate = req.params.date;
-    var resUTC, resUnix;
-    if (reqDate.includes('-')) {
-        resUTC = new Date(reqDate).toUTCString();
-        resUnix = Math.floor(new Date(reqDate).getTime());
-    } else {
-        resUTC = new Date(parseInt(reqDate)).toUTCString();
-        resUnix = Math.floor(new Date(parseInt(reqDate)).getTime());
-    }
-    res.status(200).json({ unix: resUnix, utc: resUTC })
+    if (reqDate.includes('-')) return res.status(200).json({ unix: new Date(reqDate).getTime(), utc: new Date(reqDate).toUTCString() })
+    else return res.status(200).json({ unix: reqDate, utc: new Date(parseInt(reqDate)).toUTCString() })
 })
+
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`Server is running at port ${process.env.SERVER_PORT}`)
